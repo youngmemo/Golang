e2e7c2b8386c66f7e2c9d705ff488503b1ab44e9
Mehmet Eksi <mehmeteksi99@hotmail.com>
2022-01-28 19:33:36 +0100

    Endret slik at MyQuote kommer fra min egen lokal modul over til main isteden rett ifra eksternt

---
+++ Modul1/MyQuotes/quotes.go
@@ -0,0 +1,19 @@
+package MyQuotes
+
+import "rsc.io/quote"
+
+func PrintGo() string {
+	return quote.Go()
+}
+
+func PrintHello() string {
+	return quote.Hello()
+}
+
+func PrintGlass() string {
+	return quote.Glass()
+}
+
+func PrintOpt() string {
+	return quote.Opt()
+}
--- Modul1/main.go
+++ Modul1/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"example.com/oppgaver/MyQuotes"
 	"example.com/oppgaver/RiverCrossing"
 	"fmt"
-	"rsc.io/quote"
 )
 
 func main() {
-	fmt.Println(quote.Go())
-	fmt.Println(quote.Hello())
-	fmt.Println(quote.Glass())
-	fmt.Println(quote.Opt())
+	fmt.Println(MyQuotes.PrintGo())
+	fmt.Println(MyQuotes.PrintGlass())
+	fmt.Println(MyQuotes.PrintHello())
+	fmt.Println(MyQuotes.PrintOpt())
 
 	fmt.Println(RiverCrossing.ViewState())
 	RiverCrossing.PutInFox()
